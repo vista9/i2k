bd2eb6d2baedaa534d211971329aee6344104804
vista9 <181370512+vista9@users.noreply.github.com>
2025-09-03 15:44:51 +0500

    fix

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -3,25 +3,20 @@
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
 
-n1 = int(input("Введи первое число\n> "))
-n2 = int(input("Введи второе число\n> "))
-operation = input("Какую операцию хочешь выполнить? [плюс, минус, умнж, дел]\n> ")
+n1 = int(input("Введи первое число: "))
+n2 = int(input("Введи второе число: "))
+operation = input("Какую операцию хочешь выполнить? [+, -, *, /]: ")
 
-if operation == "плюс":
-    answer = n1 + n2
-    print(f">> Результат: {answer}")
-    print(f">> Тип переменной answer: {type(answer)}")
-elif operation == "минус":
-    answer = n1 - n2
-    print(f">> Результат: {answer}")
-    print(f">> Тип переменной answer: {type(answer)}")
-elif operation == "умнж":
-    answer = n1 * n2
-    print(f">> Результат: {answer}")
-    print(f">> Тип переменной answer: {type(answer)}")
-elif operation == "дел":
-    answer = n1 / n2
-    print(f">> Результат: {answer}")
-    print(f">> Тип переменной answer: {type(answer)}")
+if operation in ['+', '-', '*', '/']:
+    if operation == "+":
+        answer = n1 + n2
+    elif operation == "-":
+        answer = n1 - n2
+    elif operation == "*":
+        answer = n1 * n2
+    elif operation == "/":
+        answer = n1 / n2
+    print(answer)
+    print(type(answer))
 else:
-    print(">> Указана некорректная операция")
+    print(">> [ОШИБКА] Указана некорректная операция")
