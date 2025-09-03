d10381e1c75b5a5a0b82040f4a6049a13ec47ca8
vista9 <181370512+vista9@users.noreply.github.com>
2025-09-03 16:32:41 +0500

    add try/catch construction

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -3,25 +3,31 @@
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
 
-n1 = int(input("Введи первое число: "))
-n2 = int(input("Введи второе число: "))
-
+n1 = input("Введи первое число: ")
+n2 = input("Введи второе число: ")
 operations = ['+', '-', '*', '/']
-operation = input(f"Какую операцию хочешь выполнить? ({', '.join(operations)}): ")
 
-if operation in operations:
-    if operation == operations[0]:
-        answer = n1 + n2
-    elif operation == operations[1]:
-        answer = n1 - n2
-    elif operation == operations[2]:
-        answer = n1 * n2
-    elif operation == operations[3]:
-        if n2 == 0:
-            answer = 0
-        else:
-            answer = n1 / n2
-    print(answer)
-    print(type(answer))
+try:
+    n1 = int(n1)
+    n2 = int(n2)
+except:
+    print(">> [ОШИБКА] Нужно указать число")
 else:
-    print(f">> [ОШИБКА] Указана некорректная операция ({', '.join(operations)})")
+    operation = input(f"Какую операцию хочешь выполнить? ({', '.join(operations)}): ")
+    if operation in operations:
+        if operation == operations[0]:
+            answer = n1 + n2
+        elif operation == operations[1]:
+            answer = n1 - n2
+        elif operation == operations[2]:
+            answer = n1 * n2
+        elif operation == operations[3]:
+            if n2 == 0:
+                answer = 0
+            else:
+                answer = n1 / n2
+    
+        print(answer)
+        print(type(answer))
+    else:
+        print(f">> [ОШИБКА] Указана некорректная операция ({', '.join(operations)})")
